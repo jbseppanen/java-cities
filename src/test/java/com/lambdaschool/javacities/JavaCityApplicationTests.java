6967439f33d6833b0a09f5a5ea2e462f1187cc81
Jacob Seppanen <jbseppanen@gmail.com>
2019-02-13 11:51:17 -0800

    Add City Class.

--- src/test/java/com/lambdaschool/javacities/JavaCityApplicationTests.java
+++ src/test/java/com/lambdaschool/javacities/JavaCityApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class JavaCitiesApplicationTests {
+public class JavaCityApplicationTests {
 
     @Test
     public void contextLoads() {
